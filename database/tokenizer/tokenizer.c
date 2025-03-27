410ab132e2186c7ebd1fd52c7b77e896ed0e2923
Arup3201 <arupjana7365@gmail.com>
2025-03-27 19:01:39 +0000

    tokenizer updated

--- database/tokenizer/tokenizer.c
+++ database/tokenizer/tokenizer.c
@@ -1,8 +1,23 @@
 #include<stdio.h>
+#include<fcntl.h>
 
+void tokenize(const char *fContent) {
+
+}
 
 int main(int argc, char *argv[]) {
-	
+	if(argc < 2) {
+		fprintf(stderr, "Usage: %s filename.c\n", argv[0]);
+		return 1;
+	}
+
+	const char *tFilename = argv[1];
+	fprintf(stdout, "[INFO] Starting the tokenization of %s\n", tFilename);
+
+	int fd = open(tFilename, O_RDWR | O_CREAT);
+	char *buf;
+
+	tokenize(buf);
 
 	return 1;
 }
