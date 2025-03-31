62f5d7d392aa8905cd4920e2ecead22bf4d605e0
Arup3201 <arupjana7365@gmail.com>
2025-03-31 17:34:07 +0000

    tokenizer character classification

--- database/tokenizer/tokenizer.c
+++ database/tokenizer/tokenizer.c
@@ -6,6 +6,23 @@
 
 #define MAX_BUFFER_SIZE 1024
 
+enum CharacterClass {
+	WHITESPACE, 
+	ALPHABETIC, 
+	NUMERIC, 
+	ALPHANEUMERIC, 
+	HEX, 
+	SPECIAL,
+} ;
+
+typedef enum CharacterClass CharacterClass;
+
+CharacterClass getCharacterClass(wchar_t uchar) {
+	if(uchar == 0x009 || uchar == 0x00a || uchar == 0x00c || uchar == 0x00d || uchar == 0x020) {
+		return WHITESPACE;
+	}
+}
+
 int main(int argc, char *argv[]) {
 	if(argc < 2) {
 		fprintf(stderr, "Usage: %s filename.c\n", argv[0]);
