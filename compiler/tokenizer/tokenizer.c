b90f78e388d0ff04d616a13c9db0649fd1c6d7f8
Arup3201 <arupjana7365@gmail.com>
2025-04-11 17:12:26 +0000

    update: tokenizer with 2 buffer scheme

--- compiler/tokenizer/tokenizer.c
+++ compiler/tokenizer/tokenizer.c
@@ -3,30 +3,52 @@
 #include<unistd.h>
 #include<stdbool.h>
 #include<stdlib.h>
+#include<string.h>
 
 #include "tokenizer.h"
 
 typedef struct Tokenizer Tokenizer;
 typedef enum TokenType TokenType;
 
-void initTokenizer(Tokenizer *t, char *fname) {
+Tokenizer* getTokenizer(char *fname) {
 	Tokenizer *tokenizer = (Tokenizer*)malloc(sizeof(Tokenizer));
+
 	tokenizer->tFd = open(fname, O_RDONLY, S_IRUSR);
+	read(tokenizer->tFd, tokenizer->buffers[0], BUFFER_SIZE);
+	tokenizer->buffers[0][BUFFER_SIZE] = SENTINEL;
+
 	tokenizer->active = 0;
-	read(tokenizer->tFd, tokenizer->buffers[tokenizer->active], BUFFER_SIZE);
 	tokenizer->lexemeBegin = tokenizer->buffers[tokenizer->active];
 	tokenizer->forward = tokenizer->lexemeBegin;
+
+	return tokenizer;
 }
 
-void destroyTokenizer(Tokenizer *t) {
-	close(t->tFd);
-	t->lexemeBegin = NULL;
-	t->forward = NULL;
-	free(t);
+void reloadBuffer(Tokenizer *t) {
+	*t->buffers[t->active] = SENTINEL;
+
+	t->active = 1 - t->active;
+	read(t->tFd, t->buffers[t->active], BUFFER_SIZE);
+}
+
+void moveForward(Tokenizer *t) {
+	if(*(t->forward+1) == SENTINEL) {
+		reloadBuffer(t);
+		t->forward = t->buffers[t->active];
+	} else {
+		t->forward++;
+	}
 }
 
 bool isSpace(char ch) {
 	return ch == ' ';
+}
+
+void freeTokenizer(Tokenizer *t) {
+	close(t->tFd);
+	t->lexemeBegin = NULL;
+	t->forward = NULL;
+	free(t);
 }
 
 int main(int argc, char* argv[]) {
@@ -35,11 +57,15 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
-	Tokenizer tokenizer;
-	initTokenizer(&tokenizer, argv[1]);
-	
-	
+	Tokenizer *tokenizer;
+	tokenizer = getTokenizer(argv[1]);	
 
-	destroyTokenizer(&tokenizer);
+	while(*tokenizer->forward != SENTINEL) {
+		printf("%c", *tokenizer->forward);
+
+		moveForward(tokenizer);
+	}
+
+	freeTokenizer(tokenizer);
 	return 0;
 }
